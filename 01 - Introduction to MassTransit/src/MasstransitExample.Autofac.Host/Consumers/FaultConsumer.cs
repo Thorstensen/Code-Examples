4407431c8d254f37835750e40f045dca82e2a1e2
Tobias Moe Thorstensen <tobias@zizr.com>
2020-06-15 19:56:36 +0200

    added fault consumer

--- 01 - Introduction to MassTransit/src/MasstransitExample.Autofac.Host/Consumers/FaultConsumer.cs
+++ 01 - Introduction to MassTransit/src/MasstransitExample.Autofac.Host/Consumers/FaultConsumer.cs
@@ -1,7 +1,15 @@
+using System.Threading.Tasks;
+using MassTransit;
+using MasstransitExample.Autofac.Host.Events;
+
 namespace MasstransitExample.Autofac.Host.Consumers
 {
-    public class FaultConsumer
+    public class FaultConsumer : IConsumer<Fault<IEvent>>
     {
-        
+        public Task Consume(ConsumeContext<Fault<IEvent>> context)
+        {
+            //This Consumer will get triggered once a message is moved to the queueName_error queue.
+            return Task.CompletedTask;
+        }
     }
 }
