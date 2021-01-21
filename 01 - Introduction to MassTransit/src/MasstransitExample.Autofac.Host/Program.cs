61dd50490caefed03a24a360f3e3570e6be5d766
Tobias Moe Thorstensen <tobias@enso.no>
2021-01-21 14:01:12 +0100

    updated main to handle user input and readme to reflect the correct docker commands

--- 01 - Introduction to MassTransit/src/MasstransitExample.Autofac.Host/Program.cs
+++ 01 - Introduction to MassTransit/src/MasstransitExample.Autofac.Host/Program.cs
@@ -17,23 +17,29 @@ namespace MasstransitExample.Autofac.Host
             var container = containerBuilder.Build();
         
             var busControl = container.Resolve<IBusControl>();
-            
             Console.WriteLine("Starting bus");
             await busControl.StartAsync();
             Console.WriteLine("Bus has started");
 
-            var numberOfMessagesToPublish = 100;
-            for (var i = 0; i < numberOfMessagesToPublish; i++)
-            {
-                var msg = new SomethingCrazyHappendEvent
-                {
-                    What = RandomWordGenerator.Generate()
-                };
+            // var numberOfMessagesToPublish = 100;
+            // for (var i = 0; i < numberOfMessagesToPublish; i++)
+            // {
+            //     var msg = new SomethingCrazyHappendEvent
+            //     {
+            //         What = RandomWordGenerator.Generate()
+            //     };
                 
-                await busControl.Publish(msg);
-            }
+            //     await busControl.Publish(msg);
+            // }
             
-            Console.WriteLine($"Finished publishing {numberOfMessagesToPublish} events to RabbitMQ. Bye!");
+            // Console.WriteLine($"Finished publishing {numberOfMessagesToPublish} events to RabbitMQ. Bye!");
+            string command = string.Empty;
+            while(!command.Equals("q", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.WriteLine("Please enter a command:");
+                command = Console.ReadLine();
+                Console.WriteLine("You wrote: " + command);
+            }
         }
     }
 }
